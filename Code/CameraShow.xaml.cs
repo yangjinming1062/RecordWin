f48d2a0ea880ad9cd9cc916905b17aedd4ae253d
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-01-21 08:58:06 +0800

    窗体大小拖拽

--- Code/CameraShow.xaml.cs
+++ Code/CameraShow.xaml.cs
@@ -26,127 +26,10 @@ namespace RecordWin
         {
             InitializeComponent();
             FileName = fileName;
-            ShowShell();
             BeginRecord();
         }
 
-        private void BeginRecord()
-        {
-            if (!string.IsNullOrEmpty(SettingHelp.Settings.摄像头Key) && SettingHelp.Settings.摄像头参数 > -1)//实例化设备控制类
-            {
-                Camera = new VideoCaptureDevice(SettingHelp.Settings.摄像头Key);
-                //配置录像参数(宽,高,帧率,比特率等参数)VideoCapabilities这个属性会返回摄像头支持哪些配置
-                Camera.VideoResolution = Camera.VideoCapabilities[SettingHelp.Settings.摄像头参数];
-                imgCamera.Width = Camera.VideoResolution.FrameSize.Width;
-                imgCamera.Height = Camera.VideoResolution.FrameSize.Height;
-                Camera.NewFrame += Camera_NewFrame;//设置回调,aforge会不断从这个回调推出图像数据
-                Camera.Start();//打开摄像头
-
-                if (!SettingHelp.Settings.桌面)
-                {
-                    lock (this) //打开录像文件(如果没有则创建,如果有也会清空),这里还有关于
-                    {
-                        VideoOutPut = new VideoFileWriter();
-                        VideoOutPut.Open(FileName, Camera.VideoResolution.FrameSize.Width, Camera.VideoResolution.FrameSize.Height,
-                           Camera.VideoResolution.AverageFrameRate, (VideoCodec)Enum.Parse(typeof(VideoCodec), SettingHelp.Settings.编码类型),
-                           Camera.VideoResolution.FrameSize.Width * Camera.VideoResolution.FrameSize.Height * SettingHelp.Settings.视频质量);
-                    }
-                }
-                Show();
-            }
-            else
-            {
-                Close();
-            }
-        }
-
         #region 窗体大小拖拽
-        private void ShowShell()
-        {
-            //foreach (UIElement element in ResizeGrid.Children)
-            //{
-            //    if (element is Rectangle)
-            //    {
-            //        var resizeRectangle = element as Rectangle;
-            //        if (resizeRectangle != null)
-            //        {
-            //            resizeRectangle.Visibility = Visibility.Visible;
-            //            resizeRectangle.PreviewMouseDown += ResizeRectangle_PreviewMouseDown;
-            //            resizeRectangle.MouseEnter += ResizeRectangle_MouseEnter;
-            //            resizeRectangle.MouseLeave += ResizeRectangle_MouseLeave;
-            //        }
-            //    }
-            //}
-        }
-
-        private void ResizeRectangle_MouseLeave(object sender, MouseEventArgs e)
-        {
-            Cursor = Cursors.Arrow;
-        }
-
-        /// <summary>
-        /// 按下拖动区域发生事件
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ResizeRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Rectangle rectangle = sender as Rectangle;
-            if (rectangle != null)
-            {
-                switch (rectangle.Name)
-                {
-                    case "Top":
-                        Cursor = Cursors.SizeNS;
-                        ResizeWindow(ResizeDirection.Top);
-                        break;
-                    case "Bottom":
-                        Cursor = Cursors.SizeNS;
-                        ResizeWindow(ResizeDirection.Bottom);
-                        break;
-                    case "Left":
-                        Cursor = Cursors.SizeWE;
-                        ResizeWindow(ResizeDirection.Left);
-                        break;
-                    case "Right":
-                        Cursor = Cursors.SizeWE;
-                        ResizeWindow(ResizeDirection.Right);
-                        break;
-                    case "TopLeft":
-                        Cursor = Cursors.SizeNWSE;
-                        ResizeWindow(ResizeDirection.TopLeft);
-                        break;
-                    case "TopRight":
-                        Cursor = Cursors.SizeNESW;
-                        ResizeWindow(ResizeDirection.TopRight);
-                        break;
-                    case "BottomLeft":
-                        Cursor = Cursors.SizeNESW;
-                        ResizeWindow(ResizeDirection.BottomLeft);
-                        break;
-                    case "BottomRight":
-                        Cursor = Cursors.SizeNWSE;
-                        ResizeWindow(ResizeDirection.BottomRight);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
-        /// <summary>
-        /// 重绘Window
-        /// </summary>
-        /// <param name="direction"></param>
-        private void ResizeWindow(ResizeDirection direction)
-        {
-            SendMessage(((HwndSource)PresentationSource.FromVisual(this)).Handle, 0x112, (IntPtr)(61440 + direction), IntPtr.Zero);
-        }
-        /// <summary>
-        /// 拖动区域内移动前事件
-        /// </summary>
         private void ResizeRectangle_MouseEnter(object sender, MouseEventArgs e)
         {
             Rectangle rectangle = sender as Rectangle;
@@ -183,6 +66,65 @@ namespace RecordWin
                 }
             }
         }
+        private void ResizeRectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+        private void ResizeRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            if (rectangle != null)
+            {
+                switch (rectangle.Name)
+                {
+                    case "rTop":
+                        Cursor = Cursors.SizeNS;
+                        ResizeWindow(ResizeDirection.Top);
+                        break;
+                    case "rBottom":
+                        Cursor = Cursors.SizeNS;
+                        ResizeWindow(ResizeDirection.Bottom);
+                        break;
+                    case "rLeft":
+                        Cursor = Cursors.SizeWE;
+                        ResizeWindow(ResizeDirection.Left);
+                        break;
+                    case "rRight":
+                        Cursor = Cursors.SizeWE;
+                        ResizeWindow(ResizeDirection.Right);
+                        break;
+                    case "rTopLeft":
+                        Cursor = Cursors.SizeNWSE;
+                        ResizeWindow(ResizeDirection.TopLeft);
+                        break;
+                    case "rTopRight":
+                        Cursor = Cursors.SizeNESW;
+                        ResizeWindow(ResizeDirection.TopRight);
+                        break;
+                    case "rBottomLeft":
+                        Cursor = Cursors.SizeNESW;
+                        ResizeWindow(ResizeDirection.BottomLeft);
+                        break;
+                    case "rBottomRight":
+                        Cursor = Cursors.SizeNWSE;
+                        ResizeWindow(ResizeDirection.BottomRight);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+        /// <summary>
+        /// 重绘Window
+        /// </summary>
+        private void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(((HwndSource)PresentationSource.FromVisual(this)).Handle, 0x112, (IntPtr)(61440 + direction), IntPtr.Zero);
+        }
+
         public enum ResizeDirection
         {
             Left = 1,
@@ -195,6 +137,37 @@ namespace RecordWin
             BottomRight = 8,
         }
         #endregion
+
+        private void BeginRecord()
+        {
+            if (!string.IsNullOrEmpty(SettingHelp.Settings.摄像头Key) && SettingHelp.Settings.摄像头参数 > -1)//实例化设备控制类
+            {
+                Camera = new VideoCaptureDevice(SettingHelp.Settings.摄像头Key);
+                //配置录像参数(宽,高,帧率,比特率等参数)VideoCapabilities这个属性会返回摄像头支持哪些配置
+                Camera.VideoResolution = Camera.VideoCapabilities[SettingHelp.Settings.摄像头参数];
+                imgCamera.Width = Camera.VideoResolution.FrameSize.Width;
+                imgCamera.Height = Camera.VideoResolution.FrameSize.Height;
+                Camera.NewFrame += Camera_NewFrame;//设置回调,aforge会不断从这个回调推出图像数据,SnapshotFrame也是有待比较
+                Camera.Start();//打开摄像头
+
+                if (!SettingHelp.Settings.桌面)
+                {
+                    lock (this) //打开录像文件(如果没有则创建,如果有也会清空),这里还有关于
+                    {
+                        VideoOutPut = new VideoFileWriter();
+                        VideoOutPut.Open(FileName, Camera.VideoResolution.FrameSize.Width, Camera.VideoResolution.FrameSize.Height,
+                           Camera.VideoResolution.AverageFrameRate, (VideoCodec)Enum.Parse(typeof(VideoCodec), SettingHelp.Settings.编码类型),
+                           Camera.VideoResolution.FrameSize.Width * Camera.VideoResolution.FrameSize.Height * SettingHelp.Settings.视频质量);
+                    }
+                }
+                Show();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
         /// <summary>
         /// 摄像头输出回调
         /// </summary>
@@ -202,7 +175,6 @@ namespace RecordWin
         {
             if (!isParsing)
             {
-                //var bmp = eventArgs.Frame.Clone() as System.Drawing.Bitmap;
                 Dispatcher.Invoke(new Action(() =>
                 {
                     MemoryStream ms = new MemoryStream();
@@ -213,7 +185,7 @@ namespace RecordWin
                     ms.Close();
                     image.EndInit();
                     imgCamera.Source = image;
-                }));
+                }));//同步显示
                 if (!SettingHelp.Settings.桌面)
                     VideoOutPut.WriteVideoFrame(eventArgs.Frame);
             }
@@ -245,13 +217,14 @@ namespace RecordWin
                     isParsing = true;
                     System.Threading.Tasks.Task.Factory.StartNew(() =>
                     {
-                        Camera.SignalToStop();
+                        Camera.SignalToStop();//用其他的stop会长时间卡住，必须用这个
                         Camera = null;
                     });
                     //停摄像头
                     if (!SettingHelp.Settings.桌面)
                     {
                         VideoOutPut.Close();//关闭录像文件,如果忘了不关闭,将会得到一个损坏的文件,无法播放
+                        VideoOutPut.Dispose();
                     }
                     if (Owner is MainWindow)
                     {
@@ -265,11 +238,6 @@ namespace RecordWin
                 }
             }
             catch { }
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left && e.LeftButton == MouseButtonState.Pressed) DragMove();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -289,6 +257,20 @@ namespace RecordWin
                 }
                 Left = S.WorkingArea.Width - Width - 10;
                 Top = S.WorkingArea.Height - Height - 10;
+            }
+        }
+
+        private void Title_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) { return; }
+
+            if (e.ClickCount > 1)
+            {
+                WindowState = WindowState == WindowState.Maximized ? WindowState = WindowState.Normal : WindowState = WindowState.Maximized;
+            }
+            else if (e.LeftButton == MouseButtonState.Pressed && WindowState == WindowState.Normal)
+            {
+                this.DragMove();
             }
         }
     }
