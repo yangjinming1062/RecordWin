a75944e5e122f7ec3657c807288e1ed5b89410a7
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2020-06-30 16:05:39 +0800

    增加在线下载依赖包

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,12 +11,17 @@ namespace RecordWin
     {
         private void OnStartup(object sender, StartupEventArgs e)
         {
-            string zipPath = Path.Combine(AppContext.BaseDirectory, "RecordLid.zip");
-            if (File.Exists(zipPath))//依赖文件以压缩包存储，启动时检测压缩包，如果存在则解压
+            if (!Functions.CheckRecordLid())
             {
-                var SevenZip = new SevenZipNET.SevenZipExtractor(zipPath);
-                SevenZip.ExtractAll(AppContext.BaseDirectory);
-                File.Delete(zipPath);//解压完成移除压缩包
+                string zipPath = Path.Combine(AppContext.BaseDirectory, "RecordLid.zip");
+                if (File.Exists(zipPath))//依赖文件以压缩包存储，启动时检测压缩包，如果存在则解压
+                {
+                    Functions.UnZipFile(zipPath);
+                }
+                else
+                {
+                    Functions.DownRecordLid();
+                }
             }
             MainWindow win = new MainWindow();
             win.Show();
---
+++ FunctionClasses/Functions.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordWin
+{
+    public static class Functions
+    {
+        /// <summary>
+        /// 调用cmd命令
+        /// </summary>
+        public static void CMD(string args)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = string.Format("cmd.exe");
+            cmd.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            cmd.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            cmd.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            cmd.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            cmd.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            cmd.Start();//启动程序
+            cmd.StandardInput.WriteLine(args + "&exit");
+            cmd.StandardInput.AutoFlush = true;
+            cmd.StandardInput.Close();
+            cmd.BeginOutputReadLine();
+            cmd.BeginErrorReadLine();
+            cmd.WaitForExit();
+            cmd.Close();
+        }
+
+        #region 下载依赖包
+        private static FileStream writeStream = null;
+        private static Stream readStream = null;
+        /// <summary>
+        /// 下载录屏依赖文件
+        /// </summary>
+        public static bool DownRecordLid()
+        {
+            bool flag;
+            string recordZipPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RecordLid.7z");
+            string recordLibURL = @"https://upgrade.1shitou.cn/pdt/app/pc/setup/Plus/RecordLid.zip";
+            try
+            {
+                flag = DownloadUrl(recordLibURL, recordZipPath) && UnZipFile(recordZipPath);//成功下载并解压返回true
+            }
+            catch (Exception ex)
+            {
+                flag = false;
+            }
+            if (File.Exists(recordZipPath))
+                File.Delete(recordZipPath);
+            return flag && CheckRecordLid();
+        }
+        /// <summary>
+        /// 检测必要依赖文件是否已经存在
+        /// </summary>
+        public static bool CheckRecordLid()
+        {
+            return File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avcodec-53.dll"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avdevice-53.dll"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avfilter-2.dll"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avformat-53.dll"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avutil-51.dll"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg.exe"))
+                && File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "swscale-2.dll"));//检查必要的文件是否已经存在了，如果都存在了则认为成功
+        }
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        private static bool DownloadUrl(string url, string localfile)
+        {
+            // 判断要下载的文件夹是否存在
+            if (File.Exists(localfile)) return true;
+            bool flag = false;
+
+            if (!Directory.Exists(localfile)) Directory.CreateDirectory(Path.GetDirectoryName(localfile));
+
+            try
+            {
+                for (int tryTime = 0; tryTime < 3; tryTime++)
+                {
+                    HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);// 打开网络连接
+                    myRequest.Timeout = 10000;
+                    var rsp = (HttpWebResponse)myRequest.GetResponse();
+                    if (rsp.StatusCode == HttpStatusCode.OK && rsp.ContentLength > 0)
+                    {
+                        if (rsp.ContentLength == 745) return false;
+                        writeStream = new FileStream(localfile, FileMode.Create);// 文件不保存创建一个文件
+
+                        readStream = rsp.GetResponseStream();// 向服务器请求,获得服务器的回应数据流
+                        byte[] btArray = new byte[10240];// 定义一个字节数据,用来向readStream读取内容和向writeStream写入内容
+                        int contentSize = readStream.Read(btArray, 0, btArray.Length);// 向远程文件读第一次
+                        long currPostion = 0;//long startPosition = 0; // 上次下载的文件起始位置
+                        while (contentSize > 0)// 如果读取长度大于零则继续读
+                        {
+                            currPostion += contentSize;
+                            int percent = (int)(currPostion * 100 / rsp.ContentLength) - 1;
+                            writeStream.Write(btArray, 0, contentSize);// 写入本地文件
+                            contentSize = readStream.Read(btArray, 0, btArray.Length);// 继续向远程文件读取
+                        }
+                        //关闭流
+                        writeStream.Close();
+                        readStream.Close();
+
+                        flag = true;        //返回true下载成功
+                        break;
+                    }
+                    rsp.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                writeStream?.Close();
+                if (File.Exists(localfile))
+                    File.Delete(localfile);
+                flag = false;//返回false下载失败
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 解压文件
+        /// </summary>
+        public static bool UnZipFile(string filePath)
+        {
+            var flag = true;
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var SevenZip = new SevenZipNET.SevenZipExtractor(filePath);
+                    SevenZip.ExtractAll(AppContext.BaseDirectory);
+                    File.Delete(filePath);//解压完成移除压缩包
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return flag;
+        }
+        #endregion
+    }
+}
